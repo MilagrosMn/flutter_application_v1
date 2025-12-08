fd7cff66ea976b548a577eabc23126fa97d0a3de
Milagros Muñoz Nicosia <98249051+MilagrosMn@users.noreply.github.com>
2025-12-08 15:45:21 -0300

    Update routes.dart

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/form_screen.dart';
 import '../screens/screens.dart';
 
 class Routes {
   static const home = '/';
   static const listado = '/listado';
   static const detalle = '/detalle';
+  static const form = '/form';
 }
 
 final Map<String, WidgetBuilder> appRoutes = {
   Routes.home: (_) => const HomeScreen(),
   Routes.listado: (_) => const ListadoScreen(),
   Routes.detalle: (_) => const DetalleScreen(),
+  Routes.form: (_) => const FormScreen(),
 };
